3d50206934d8ca52c2f5d3a29050b92788840de0
Bryce <ImperialEagle1998@gmail.com>
2020-11-12 17:57:46 -0600

    A rudimentry Linq Project

---
+++ LinqPractice/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace LinqPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> Video_Game_Titles = new List<string> { "Call of Duty World at War", "Locomotion", "Railroad Tycoon III", "Europa Universalis IV", "Crusader Kings III", "Hearts of Iron Darkest Hour" };
+
+            var OrderByLength = Video_Game_Titles.OrderBy(titles => titles.Length);
+
+           foreach(var value in OrderByLength)
+            {
+                Console.WriteLine(value);
+            }
+
+
+        }
+    }
+}
